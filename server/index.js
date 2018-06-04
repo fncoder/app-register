4bc3902031a7dff962bed2ae12a3d2f4a8e86c8f
unknown <fncoder@gmail.com>
2018-06-04 16:23:44 +0200

    add gzip compression

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const compression = require('compression');
 const bodyParser = require('body-parser');
 const register = require('./routes/register.js');
 const login = require('./routes/login.js')
@@ -7,6 +8,11 @@ const post = require('./routes/post.js');
 const staticFiles = __dirname + './../build';
 
 app.use(express.static(staticFiles));
+app.use(function(req, res, next){
+  res.set('Accept-Encoding', 'gzip');
+  next();
+});
+app.use(compression());
 app.use(bodyParser());
 app.use('/register', register.router);
 app.use('/login', login);
